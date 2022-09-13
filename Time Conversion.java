6f15f9fc7739e9a35d2cc5cc6971a5531dff1f3a
yahiamostafa <61708947+yahiamostafa@users.noreply.github.com>
2022-09-14 01:24:45 +0200

    Create Time Conversion.java

---
+++ Time Conversion.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String timeConversion(String s) {
+    // Write your code here
+    
+        if (s.substring(0 , 2) .equals("12"))
+        {
+            if (s.charAt(s.length() - 2) == 'A')
+                return "00" + s.substring(2 , s.length() - 2);
+            else
+                return s.substring(0 , s.length() - 2);
+        }
+        
+        if (s.charAt(s.length() - 2) == 'A')
+        {
+            return s.substring(0 , s.length() - 2);
+            
+        }else
+        {
+            return Integer.parseInt(s.substring(0 , 2)) + 12 + "" +s.substring(2 , s.length() - 2);
+        }
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.timeConversion(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
